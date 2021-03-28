550bc5d1b13849d1810492eaaddca4d9ace2cc1d
jywng <ngjaywa@gmail.com>
2021-03-28 11:56:03 -0400

    asdf

--- components/calc.js
+++ components/calc.js
@@ -4,15 +4,10 @@ class Calc extends React.Component {
     render() {
         
         const aim = Math.ceil((100 - this.props.battery) / 5)
-        //const this.props.time
-        //this.props.json should be ML prediction
+
         const keys = Object.keys(this.props.json)
         const ret = []
-        //based on time, and predicted solar forecast, calculate the cheapest way to 
-        //battery percentages
-        //use ML with weather data to predict amount of free energy from solar
-        //use greedy algorithm to take power from grid at cheapest times otherwise
-        //for (const p in keys) 
+
         for (var i = 0; i < aim; i++) {
             const price = this.props.json[keys[i]]
             ret.push(   <tr>
